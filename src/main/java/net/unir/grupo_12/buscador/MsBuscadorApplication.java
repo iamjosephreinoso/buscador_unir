3d09c6f32fcce77a3274deb00efaf1b52d83ccd8
iamjosephreinoso <jreinosov@est.ups.edu.ec>
2024-06-13 08:55:03 -0500

    Eureka 2

--- src/main/java/net/unir/grupo_12/buscador/MsBuscadorApplication.java
+++ src/main/java/net/unir/grupo_12/buscador/MsBuscadorApplication.java
@@ -3,9 +3,13 @@ package net.unir.grupo_12.buscador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.netflix.eureka.*;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableJpaRepositories(basePackages = "net.unir.grupo_12.buscador.repository")
 @EntityScan(basePackages = "net.unir.grupo_12.buscador.entity")
 public class MsBuscadorApplication {
